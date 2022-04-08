0492b3a1c8b86397375c715ebc0ce7d6038b6a6d
Vishnu Msd <vishnumsd99@gmail.com>
2022-04-08 11:11:36 +0530

    ed-website

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('this is education univercity website');
+
+
+/////////menu hidden and visible///////
+
+let navLinks = document.getElementById('navLink');
+
+function showMenu(){
+    navLinks.style.right = "0px";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-300px";
+}
